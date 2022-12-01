597ea6ad362cc844aa5306f2895b753f2657ca6b
Ayden Diel <aydendiel@gmail.com>
2022-12-01 13:17:03 -0500

    Tweaked popup more

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,36 @@
 import SiteScraper from './scripts/background_modules/SiteScraper.js'
 import {tabIsOpen} from './scripts/background_modules/tabFunctions.js';
 
+const sendUpdateMessage = (msg, finished = false) => {
+    chrome.runtime.sendMessage({
+        type: 'update',
+        message: msg,
+        finished: finished
+    });
+}
+
 chrome.runtime.onInstalled.addListener(() => {
     console.log('<3')
 });
 
 chrome.runtime.onMessage.addListener((req, sender, sendResponse)=>{
-    if(req.message == 'start'){
+    if(req.type == 'startScript'){
         console.log('Good morning, Ayden!')
         console.log('starting script...')
         main()
-        .then((message)=>{
-            console.log(message.message)
-        });
+            .then((result)=>{
+                console.log(result);
+                sendUpdateMessage(result.userMessage, true);
+            })
     }
-})
+});
 
 //fills out spreadsheet with all needed info
 const main = async () => {
+
+    sendUpdateMessage('Script started...');
+
+    const startTime = Date.now();
 
     const spreadSheetURL = 'https://docs.google.com/spreadsheets/d/1C7BUsezoUFqRsp2SzK7I7XorhBda2_fmpiEeeB9Mmt8/edit#gid=0';
     const otherSites = [
@@ -32,7 +45,7 @@ const main = async () => {
     console.log(isOpen)
 
     if(!isOpen){
-        chrome.tabs.create(
+        await chrome.tabs.create(
             {active: false,
                 index: 0,
                 pinned: true,
@@ -40,6 +53,8 @@ const main = async () => {
         );
     }
 
-    return {message: 'success'}
+    const executionTime = (Date.now() - startTime) / 1000; //in seconds
+
+    return {successful: true, userMessage: `Script completed in ${executionTime} seconds.`}
 
 }
--- extension/src/main.js
+++ extension/src/main.js
@@ -1,12 +1,27 @@
 const startButton = document.querySelector('#startButton');
+const updateText = document.querySelector('#messageText');
 
-const sendStart = () => {
+const start = () => {
     chrome.runtime.sendMessage(
-        message = {message: "start"}
+        message = {type: "startScript"}
     )
+    startButton.disabled = true;
 }
 
-startButton.onclick = sendStart;
+const finish = () => {
+    startButton.disabled = false;
+}
+
+chrome.runtime.onMessage.addListener((message)=>{
+    if(message.type == 'update'){
+        messageText.innerHTML = message.message;
+        if(message.finished){
+            finish();
+        }
+    }
+});
+
+startButton.onclick = start;
 
 
 console.log(startButton.onclick)
